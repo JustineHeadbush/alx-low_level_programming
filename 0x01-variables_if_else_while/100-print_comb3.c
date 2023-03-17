c23fc625ed089dfbd7374b83b7afa9979d6f84e3
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-17 08:35:43 -0700

    C Variables advanced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description: prints all possible different combinations of two digits.
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+	for (m = 0; m < 10; m++)
+	{
+		putchar((n % 10) + '0');
+		putchar((m % 10) + '0');
+
+		if (n == 9 && m == 9)
+			continue;
+	}
+	}
+		putchar(',');
+		putchar(' ');
+
+		putchar('\n');
+		return (0);
+
+}
