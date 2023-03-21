2ab29b1159183ac874d64ba20e41a14b9503e77d
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-21 00:33:10 -0700

    checks for +-0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,9 @@
 /**
  * print_sign - prints the sign of a number
  * @n: number to be checked
- * Return:  1 if n is greater than zero.
- * 0 if n is zero.
- * -1 if n is less than zero
+ * Return: 1 if n is greater than zero.
+ *	0 if n is zero.
+ *	-1 if n is less than zero
  */
 int print_sign(int n)
 {
@@ -15,7 +15,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
+		_putchar('0');
 		return (0);
 	}
 	else
