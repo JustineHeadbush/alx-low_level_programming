41fa0b3ff88747d1356d93145338a47fd9bcf425
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-21 05:09:42 -0700

    Fibonacci 3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 98 Fibonacci numbers, starting with 1 and 2,
+ *        followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count;
+	int num1 = 0, num2 = 1, res;
+
+	for (count = 0; count <= 98; count++)
+	{
+		res = num1 + num2;
+		printf("%d", res);
+
+		num1 = num2;
+		num2 = res;
+
+		if (count == 99)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
