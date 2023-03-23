c77646d779bc3bb257d48a9e47ed554d32103532
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-23 03:08:45 -0700

    Fixed function description

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers: function that prints the numbers, from 0 to 9,
+ * print_numbers - function that prints the numbers, from 0 to 9,
  * followed by a new line
  */
 void print_numbers(void)
