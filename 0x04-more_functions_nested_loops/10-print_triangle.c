9a0f418956ea3a1ae0664ac35da6b7afac346de5
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-23 06:22:33 -0700

    Fixed the shape of the triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,23 +7,22 @@ void print_triangle(int size)
 {
 	int num1;
 	int num2;
-	int num3;
 
 	if (size > 0)
 	{
 		for (num1 = 1; num1 <= size; num1++)
 		{
-			for (num2 = num1; num2 < size; num2++)
-			{
+			for (num2 = size - num1; num2 > 0; num2--)
 				_putchar(' ');
-			}
-			for (num3 = 1; num3 <= num1; num3++)
-			{
-				_putchar('#');
-			}
-		}
 
-		_putchar('\n');
+			for (num2 = 0; num2 < num1; num2++)
+				_putchar('#');
+
+			if (num1 == size)
+				continue;
+
+			_putchar('\n');
+		}
 	}
 
 	_putchar('\n');
