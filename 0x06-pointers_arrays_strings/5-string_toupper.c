2ff422810b6629292a00c52feada662c25cbeb51
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-29 09:34:59 -0700

    change to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - converts lowercase to uppercase
+ * @str: the string
+ * Return: always 0
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+
+		i++;
+	}
+
+	return (0);
+}
