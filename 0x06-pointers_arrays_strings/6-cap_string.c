9a8444840fc581c6e07f40d61b0471be644e3cb4
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-29 09:59:08 -0700

    capitalize the string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
- * string_toupper - converts lowercase to uppercase
+ * cap_string - capitalizes the string
  * @str: the string
- * Return: always 0
+ * Return: 0
  */
-char *string_toupper(char *str)
+char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
-
-		i++;
 	}
 
+	i++;
 	return (0);
 }
