b2fcf1b30efb184da1162e189ce1b21067e4dad8
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-30 02:07:31 -0700

    testing 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * leet - that encodes a string into 1337
+ * @s: the string that will be encoded
+ *
+ * Return: @s
+ */
+char *leet(char *s)
+}
+	int n = 0, i;
+	int lower[] = {97, 101, 111, 116, 108};
+	int upper[] = {65, 69, 79, 84, 76};
+	int num[] = {52, 51, 48, 55, 49};
+
+	while (*(s + n) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(s + n) == lower[i] || *(s + n) == upper[i])
+			{
+				*(s + n) = num[i];
+				break;
+			}
+		}
+		n++;
+	}
+
+	return (s);
+}
+
