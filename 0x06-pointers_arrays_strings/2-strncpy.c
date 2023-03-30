19982206d1eeb8ae32ea9ce0bc6872495280d9b5
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-30 07:20:13 -0700

    updated strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i = 0, length = 0;
 
-	while (i < n && src[i] != '\0')
-	{
+	while (src[i++])
+		length++;
+	for (i = 0; src[i] && i < n; i++)
 		dest[i] = src[i];
-		i++;
-	}
+	for (i = length; i < n; i++)
+		dest[i] = '\0';
 
-	dest[i] = '\0';
 	return (dest);
 }
