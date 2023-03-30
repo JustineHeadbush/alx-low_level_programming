9216656f8e2cf39feac47a4caaedad2d87105b24
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-30 01:26:05 -0700

    changed return value to str in 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,21 +2,21 @@
 /**
  * string_toupper - converts lowercase to uppercase
  * @str: the string
- * Return: always 0
+ * Return: always @str
  */
 char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
+		i++;
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
 		}
 
-		i++;
 	}
 
-	return (0);
+	return (str);
 }
