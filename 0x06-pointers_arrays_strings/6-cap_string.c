20a14daf50e2e511c9af74ddb5af08000f78f0da
JustineHeadbush <justleratoheadbush@gmail.com>
2023-03-30 01:15:50 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,20 +2,30 @@
 /**
  * cap_string - capitalizes the string
  * @str: the string
- * Return: 0
+ * Return: @str
  */
 char *cap_string(char *str)
 {
 	int i = 0;
 
 	while (str[i] != '\0')
+		i++;
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (i == 0)
 		{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		if (str[i] == ' ')
+		i++;
+		{
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
 		}
+		}
+		else if
+			(str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
 	}
-
-	i++;
-	return (0);
+	return (str);
 }
+
