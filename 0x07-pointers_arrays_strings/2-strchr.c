6679cda4d15b4627c92448836aacfc5d5a3fe67d
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-03 08:08:47 -0700

    changed != to >= in 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,10 @@ char *_strchr(char *s, char c)
 {
 	int num;
 
-	for (num = 0; s[num] != '\0'; num++)
+	for (num = 0; s[num] >= '\0'; num++)
 	{
 		if (s[num] == c)
 			return (&s[num]);
 	}
-	return (0);
+	return ('\0');
 }
