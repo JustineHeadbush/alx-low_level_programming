8b6915b27d5f09eb7f1438fdc816d28bf86ef112
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-03 13:28:43 -0700

    needle in a haystack 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: string that will be searched
+ * @needle: the string that will be searched for
+ * Return: a pointer to the beginning of the located substring,
+ * NULL if there is no match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int num1, num2;
+
+	for (num1 = 0; needle[num1] != '\0'; num1++)
+	{
+		for (num2 = 0; haystack[num2] != '\0'; num2++)
+		{
+			if (needle[num1] == haystack[num2])
+				return (needle + num1);
+		}
+	}
+	return (0);
+}
