e60a3faf37dab9bf7a274d0ce60d59eaf137f19d
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-03 14:43:11 -0700

    print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,14 +8,14 @@ void print_chessboard(char (*a)[8])
 	int num1 = 0, num2 = 0;
 
 	while (num1 < 8)
+		num2 = 0;
 	{
-		num1++;
-
 		while (num2 < 8)
 		{
+			_putchar(a[num1][num2]);
 			num2++;
 		}
-		_putchar(a[num1][num2]);
+		_putchar('\n');
+	num1++;
 	}
-	_putchar('\n');
 }
