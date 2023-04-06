08d810f29c5dd77a0b24a03512fd0fe230d00f9c
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-06 04:46:00 -0700

    first attempt palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,34 @@
 #include "main.h"
 /**
+ * find_length_of_string - measures the length of the string
+ * @s: the string that will be measured
+ * Return: the length of the string
+ */
+int find_length_of_string(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+		return (find_length_of_string(s + 1));
+}
+
+/**
  * find_palindrome - finds out if a string is a palindrome
  * @s: the string that will be checked
  * @a: the left index
  * @b: the right index
+ * Return: 1 if string is palindrome, otherwise return 0
  */
 int find_palindrome(int a, int b, char *s)
 {
-	int a = 0, int b = n - 1;
+	int n = find_length_of_string(s + 1);
+	int a = 0;
+	int b = n - 1;
 
 	if (a == b || a < b)
-	{	
+	{
 		return (1);
 	}
 	else if (*s[a] != *s[b])
@@ -18,9 +36,11 @@ int find_palindrome(int a, int b, char *s)
 		return (0);
 	}
 	return (find_palindrome(a + 1, b - 1));
+}
 /**
  * is_palindrome - checks if a string is a palindrome
  * @s: the string that will be checked
+ * Return: 1 if string is palindrome, otherwise return 0
  */
 int is_palindrome(char *s)
 {
