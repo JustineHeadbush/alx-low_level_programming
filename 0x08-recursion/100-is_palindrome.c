0ff2c4ec51ca53784da020ea7eb8caee69344b92
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-06 11:55:15 -0700

    2nd attempt palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,16 +2,22 @@
 /**
  * find_length_of_string - measures the length of the string
  * @s: the string that will be measured
- * Return: the length of the string
+ * Return: length of string
  */
 int find_length_of_string(char *s)
 {
+	int n = 0;
+
 	if (*s == '\0')
 	{
 		return (0);
 	}
-	else
-		return (find_length_of_string(s + 1));
+	else if (*s != '\0')
+	{
+		n++;
+		s++;
+	}
+		return (n);
 }
 
 /**
@@ -23,19 +29,15 @@ int find_length_of_string(char *s)
  */
 int find_palindrome(int a, int b, char *s)
 {
-	int n = find_length_of_string(s + 1);
-	int a = 0;
-	int b = n - 1;
-
-	if (a == b || a < b)
+	if (a == b || a >= b)
 	{
 		return (1);
 	}
-	else if (*s[a] != *s[b])
+	else if (s[a] != s[b])
 	{
 		return (0);
 	}
-	return (find_palindrome(a + 1, b - 1));
+	return (find_palindrome(a + 1, b - 1, s));
 }
 /**
  * is_palindrome - checks if a string is a palindrome
@@ -44,9 +46,11 @@ int find_palindrome(int a, int b, char *s)
  */
 int is_palindrome(char *s)
 {
+	int n = find_length_of_string(s);
+
 	if (*s == '\0')
 	{
 		return (1);
 	}
-	return (find_palindrome);
+	return (find_palindrome(s, n - 1));
 }
