44a7ba9e09882e5d4a3416857ce789c562a9fec7
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-12 08:39:08 -0700

    task 2 betty update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,13 +10,10 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int i = 0;
-	int j = 0;
-	int index = 0;
+	int i = 0, j = 0, index = 0;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
@@ -25,19 +22,15 @@ char *str_concat(char *s1, char *s2)
 		j++;
 		i++;
 	}
-
 	i = 0;
 	while (s2[i] != '\0')
 	{
 		j++;
 		i++;
 	}
-
 	ptr = malloc((j + 1) * sizeof(char));
-
 	if (ptr == NULL)
 		return (NULL);
-
 	i = 0;
 	while (s1[i] != '\0')
 	{
@@ -45,7 +38,6 @@ char *str_concat(char *s1, char *s2)
 		index++;
 		i++;
 	}
-
 	i = 0;
 	while (s2[i] != '\0')
 	{
@@ -53,7 +45,6 @@ char *str_concat(char *s1, char *s2)
 		index++;
 		i++;
 	}
-
 	ptr[index] = '\0';
 
 	return (ptr);
