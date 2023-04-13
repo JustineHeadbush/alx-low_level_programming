c59ead7cb1840249e7ceeca4d6c7f9816615df77
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-13 13:59:24 -0700

    added condition to for loop task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1[i] && i < n; i++)
 		j++;
 
 	ptr = malloc((j + 1) * sizeof(char));
