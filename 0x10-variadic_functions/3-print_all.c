10b65668940718941041efc52d298cdec1a0f487
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-20 12:12:39 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,73 +2,96 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_all(const char * const format, ...);
 void my_char(va_list form);
 void my_float(va_list form);
 void my_string(va_list form);
 void my_int(va_list form);
 
-/******/
+/**
+ * my_char - prints a character
+ * @form: points to the character printed
+ */
 void my_char(va_list form)
 {
 	char ch;
+
 	ch = va_arg(form, int);
 	printf("%c", ch);
 }
-/******/
+/**
+ * my_float - prints a float
+ * @form: points to float printed
+ */
 void my_float(va_list form)
 {
 	float fl;
+
 	fl = va_arg(form, double);
 	printf("%f", fl);
 }
-/******/
+/**
+ * my_string - prints string
+ * @form: points to string printed
+ */
 void my_string(va_list form)
 {
 	char *st;
+
 	st = va_arg(form, char *);
+	if (st == NULL)
+	{
+		printf("nil");
+	}
 	printf("%s", st);
 }
-/******/
+/**
+ * my_int - prints integer
+ * @form: points to integer printed
+ */
 void my_int(va_list form)
 {
 	int in;
+
 	in = va_arg(form, int);
 	printf("%i", in);
 }
-/******/
+/**
+ * prints_all - prints anything
+ * @format: list of types of arguments passed to the function
+ * @...: variable number of arguments
+ * Description: if the string is NULL, print (nil) instead
+ * any other char should be ignored
+ */
 void print_all(const char * const format, ...)
 {
 	va_list forms;
-	char *sep = "";
-	int a = 0;
-	int b = 0;
-	print_t functions[] = {
+	char *sep = 0;
+	int a = 0, b;
+	prints_t oper[] = {
 		{"c", my_char},
 		{"f", my_float},
 		{"s", my_string},
 		{"i", my_int},
 	};
-	
+
 	va_start(forms, format);
 
-	while (format != NULL && (*(format + a)
+	while (format && format[a])
 	{
 		b = 0;
-		while (b < 4 && format[a] != *(functions[b].symbol))
+
+		while (b < 4 && format[a] != *(oper[b].sign))
 			b++;
 
 		if (b < 4)
 		{
 			printf("%s", sep);
-			functions[b].print(forms);
+			oper[b].printer(forms);
 			sep = ", ";
 		}
 
 		a++;
 	}
-
-	printf("\n");
-
 	va_end(forms);
+	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,14 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+typedef struct prints
+{
+	char *sign;
+	void (*printer)(va_list forms);
+} prints_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
