67c4e67b8f5d80774921faa06d5951a4641eb253
JustineHeadbush <justleratoheadbush@gmail.com>
2023-04-28 16:00:11 -0700

    fixed task 4 betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
- * free_list - frees a list_t list 
+ * free_list - frees a list_t list
  * @head: the list
  */
 void free_list(list_t *head)
 {
-
-	free(head);
-	free_list(head->next);
-	free(head->str);
-
 	if (head == NULL)
 	{
 		return;
 	}
+
+	free(head);
+	free_list(head->next);
+	free(head->str);
 }
